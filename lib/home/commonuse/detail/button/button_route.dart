4bb765dd10f9084cab3d87660682d1611c003ce1
zhoulinqin <1249924312@qq.com>
2020-10-29 22:45:39 +0800

    修改按钮样式

--- lib/home/commonuse/detail/button/button_route.dart
+++ lib/home/commonuse/detail/button/button_route.dart
@@ -11,7 +11,6 @@ class ButtonRoute extends StatefulWidget {
 }
 
 class _ButtonState extends State<ButtonRoute> {
-  int count = 0;
   int count2 = 0;
 
   @override
@@ -31,43 +30,45 @@ class _ButtonState extends State<ButtonRoute> {
         title: Text("按钮类控件"),
       ),
       body: new Builder(builder: (BuildContext context) {
+        //在new Builder下才能使用SnackBar
         return ListView(
           padding: EdgeInsets.all(10),
           children: <Widget>[
-            CommonButton(
-              "CommonButton $count",
+            SelfStyleButton(
+              "矩形按钮 SquareButton 按下弹出snake bar",
               MClickListener: () {
-                setState(() {
-                  count++;
-                });
+                showSnakeBar(context, '这是一个SnackBar ' + (count2++).toString());
               },
+              bgColor: Colors.blueAccent,
+              textStyle: TextStyle(color: Colors.white),
             ),
             SizedBox(
               height: 5,
             ),
-            CommonButton(
-              "CommonButton 按下弹出snake bar",
+            SelfStyleButton(
+              "矩形按钮 自适应宽度",
+              MClickListener: () {},
+              bgColor: Colors.blueAccent,
+              textStyle: TextStyle(color: Colors.white),
               fitSelf: true,
-              MClickListener: () {
-                final snackBar = new SnackBar(
-                  content: new Text('这是一个SnackBar ' + (count2++).toString()),
-                  action: new SnackBarAction(
-                      label: "撤销",
-                      onPressed: () {
-                        Scaffold.of(context).hideCurrentSnackBar();
-                      }),
-                );
-                //context要使用new Builder(builder: (BuildContext context)的
-                Scaffold.of(context).hideCurrentSnackBar();
-                Scaffold.of(context).showSnackBar(snackBar);
-              },
-            ),
-            SizedBox(
-              height: 5,
             ),
           ],
         );
       }),
     );
+  }
+
+  void showSnakeBar(BuildContext context, String tx){
+    final snackBar = new SnackBar(
+      content: new Text(tx ?? ""),
+      action: new SnackBarAction(
+          label: "撤销",
+          onPressed: () {
+            Scaffold.of(context).hideCurrentSnackBar();
+          }),
+    );
+    //context要使用new Builder(builder: (BuildContext context)的
+    Scaffold.of(context).hideCurrentSnackBar();
+    Scaffold.of(context).showSnackBar(snackBar);
   }
 }
--- lib/view/button/self_button.dart
+++ lib/view/button/self_button.dart
@@ -1,59 +1,55 @@
 import 'package:flutter/material.dart';
 
-class CommonButton extends Container {
-  String tx;
-  Function MClickListener;
-  BorderSide borderSide;
-  Color backgroundColor;
-  TextStyle textStyle;
+final double _BTN_DEFAULT_HEIGHT = 50;
+final double _BTN_DEFAULT_RADIUS = 10;
 
-  ///是否自适应
+///自定义样式按钮
+class SelfStyleButton extends Container {
+  String _tx;
+  Function MClickListener;
+  TextStyle textStyle;
+  Color bgColor;
+
+  double height;
+  double radiusValue;
+
+  ///是否自适应文字宽度
   bool fitSelf;
 
-  ///通用按钮
-  CommonButton(this.tx,
+  SelfStyleButton(this._tx,
       {this.MClickListener,
+      this.fitSelf,
       this.textStyle,
-      this.backgroundColor = const Color(0xffebebeb),
-      this.fitSelf = false,
-      this.borderSide = const BorderSide(
-        width: 1,
-        color: Color(0xffe8e8e8),
-        style: BorderStyle.solid,
-      )});
-
-  @override
-  get onPressed => MClickListener;
+      this.bgColor,
+      this.radiusValue,
+      this.height});
 
   @override
   Widget get child {
-    Widget childContainer = Container(
+    Widget childWidget = Container(
+      padding: EdgeInsets.only(left: 10, right: 10),
       alignment: Alignment.center,
-      padding: EdgeInsets.all(10),
-      decoration: ShapeDecoration(
-        color: backgroundColor,
-        shape: StadiumBorder(
-          side: borderSide,
-        ),
-      ),
+      height: (this.height == null || this.height == 0)
+          ? _BTN_DEFAULT_HEIGHT
+          : this.height,
       child: Text(
-        tx,
+        _tx,
         style: textStyle,
-        softWrap: true,
       ),
+      decoration: BoxDecoration(color: bgColor, borderRadius: BorderRadius.all(Radius.circular((radiusValue == null || radiusValue == 0)?_BTN_DEFAULT_RADIUS:radiusValue))),
     );
     return GestureDetector(
-      child: fitSelf
-          ? Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[childContainer],
-            )
-          : childContainer,
       onTap: () {
         if (MClickListener != null) {
           MClickListener();
         }
       },
+      child: (fitSelf != null && fitSelf)
+          ? Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [childWidget],
+            )
+          : childWidget,
     );
   }
 }
